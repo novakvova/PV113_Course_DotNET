20dfb0f8b5faab20fe478beab82a86d1c0155de9
novakvova <novakvova@gmail.com>
2024-03-07 19:14:13 +0200

    Add project files.

---
+++ SimpleHTTP/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using SimpleHTTP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleHTTP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5255");
+            try
+            {
+                LoginViewModel login = new LoginViewModel();
+                login.Email = "admin@gmail.com";
+                login.Password = "123456";
+                string json = JsonConvert.SerializeObject(login);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var resp = await client.PostAsync("/api/account/login", content);
+                if(resp.IsSuccessStatusCode)
+                {
+                    var result = await resp.Content.ReadAsStringAsync();
+                    MessageBox.Show(result);
+                }
+                else
+                {
+                    MessageBox.Show("Помилка", resp.StatusCode.ToString()); 
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
